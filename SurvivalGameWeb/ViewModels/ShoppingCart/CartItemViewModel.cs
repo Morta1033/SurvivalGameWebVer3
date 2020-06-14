a243fd36974b571755d67e0a661a7a04cbb4b1b8
freeblack70422 <freeblack70422@gmail.com>
2020-06-14 21:30:51 +0800

    View Models

---
+++ SurvivalGameWeb/ViewModels/ShoppingCart/CartItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurvivalGameWeb.ViewModels.ShoppingCart
+{
+    public class CartItemViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Img { get; set; }
+        public decimal? Price { get; set; }
+        public int Count { get; set; }
+        public decimal? Total => Price * Count; 
+    }
+}
