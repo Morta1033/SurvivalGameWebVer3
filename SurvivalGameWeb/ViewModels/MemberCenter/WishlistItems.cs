43431661cf6deea0e3fb3d1a05812c49d1c9c415
shan <b10502011@g.gmail.com>
2020-06-19 01:26:25 +0800

    edit member viewmodel and add HistoryQuantity

---
+++ SurvivalGameWeb/ViewModels/MemberCenter/WishlistItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurvivalGameWeb.ViewModels.MemberCenter
+{
+    public class WishlistItems
+    {
+        public string WishlistImg { get; set; }
+        public string WishlistName { get; set; }
+        public decimal WishlistPrice { get; set; }
+    }
+}
